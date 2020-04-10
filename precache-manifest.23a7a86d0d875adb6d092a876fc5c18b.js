e5bb28d6345b2bbd661f236930164e3eb166fbd1
dimail0880 <dimail4888@gmail.com>
2020-04-10 18:04:33 +0300

    Updates

---
+++ precache-manifest.23a7a86d0d875adb6d092a876fc5c18b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "54e032f03d3381a2fc23cc8f62fa97de",
+    "url": "/goit-react-hw-02-feedback/index.html"
+  },
+  {
+    "revision": "8ca053936896e1f3bf0a",
+    "url": "/goit-react-hw-02-feedback/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "cd900d69e9c1d31af314",
+    "url": "/goit-react-hw-02-feedback/static/js/2.fcb20fac.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/goit-react-hw-02-feedback/static/js/2.fcb20fac.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8ca053936896e1f3bf0a",
+    "url": "/goit-react-hw-02-feedback/static/js/main.b53b2528.chunk.js"
+  },
+  {
+    "revision": "1a2af30d5f9b5771679f",
+    "url": "/goit-react-hw-02-feedback/static/js/runtime-main.a031b346.js"
+  }
+]);
